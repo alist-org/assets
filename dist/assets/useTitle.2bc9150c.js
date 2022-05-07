89b52278b41318c3de946438978c9273843eaf0d
Xhofe <i@nn.ci>
2022-05-07 08:46:14 +0000

    upload 2.5.0 assets files

--- dist/assets/useTitle.2bc9150c.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./vendor.03ff3900.js";const r=(e,t=!1)=>{o.exports.useEffect(()=>{const s=document.title;return t?document.title=`${e} - alist`:document.title=e,()=>{document.title=s}},[])};var c=r;export{c as u};
---
+++ dist/assets/useTitle.de6d2013.js
@@ -0,0 +1,1 @@
+import{r as o}from"./vendor.53f783ab.js";const r=(e,t=!1)=>{o.exports.useEffect(()=>{const s=document.title;return t?document.title=`${e} - alist`:document.title=e,()=>{document.title=s}},[])};var c=r;export{c as u};
