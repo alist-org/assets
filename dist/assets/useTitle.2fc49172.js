a41a1b032aad67f5c552bdfb0450929e184e3d63
Xhofe <i@nn.ci>
2022-07-21 14:22:50 +0000

    upload 2.6.2 assets files

---
+++ dist/assets/useTitle.2fc49172.js
@@ -0,0 +1,1 @@
+import{r as o}from"./vendor.dd75dc04.js";const r=(e,t=!1)=>{o.exports.useEffect(()=>{const s=document.title;return t?document.title=`${e} - alist`:document.title=e,()=>{document.title=s}},[])};var u=r;export{u};
